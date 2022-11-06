b9ae4430884d46022cabb674e92290bb3f5eb511
mochunwang <mochunwang@qinsilk.com>
2022-11-07 00:43:27 +0800

    feat: init

---
+++ docs/.vitepress/config/sidebar.ts
@@ -0,0 +1,43 @@
+export default {
+    '/guide/' : [
+    {
+      text: '指南',
+      link: '/guide/install',
+      items: [
+        { text: '安装', link: '/guide/install' },
+        { text: '快速开始', link: '/guide/import' }
+      ]
+    }
+  ],
+  '/component/': [
+    {
+      text: '基础组件',
+      link: '/component/',
+      items: [
+        { text: 'Button 按钮', link: '/component/button' },
+        { text: 'Icon 图标', link: '/component/icon' },
+        { text: 'Link 链接', link: '/component/link' },
+        { text: 'Text 文字', link: '/component/text' },
+        { text: 'Container 布局容器', link: '/component/container' }
+      ]
+    },
+    {
+      text: 'Form 表单组件',
+      link: '/component/',
+      items: [
+        { text: 'Form 表单组件', link: '/component/form' }
+      ]
+    }
+  ],
+  '/resource/': [
+    {
+      text: '资源分享',
+      link: '/resource/FavoriteFromBrowser',
+      items: [
+        { text: '浏览器收藏夹公开', link: '/resource/FavoriteFromBrowser' },
+        { text: '编程实用工具', link: '/resource/ProgrammingUtilitySharing' },
+        { text: '编程实用工具', link: '/resource/Vue3UtilitySharing' }
+      ]
+    }
+  ]
+}
---
+++ docs/index.md
@@ -0,0 +1,35 @@
+---
+title: 'A Vue 3 UI Framework'
+layout: home
+
+hero:
+  name: A Vue 3 UI Framework
+  text: Vue3 组件库
+  tagline: 一个轻量级组件库，基于 vue3 + typescript，全面拥抱 vue3
+  image:
+    src: https://tianyuhao.cn/images/tyh-ui/tyh-ui2.svg
+    alt: tyh-ui2
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /guide/install
+    - theme: alt
+      text: GitHub
+      link: https://github.com/mcwmengxi
+
+features:
+  - icon: 💡
+    title: 简洁至上
+    details: ymy-ui 整体非常简洁明了，虽没有很多高级的组件，但它是使用最简单的方式达到实用的效果。
+  - icon: ⚡️
+    title: 轻量容易
+    details: 使用方便，配置简单，目录清晰，结构简单，组件轻量级，上手简单，像复制粘贴一样容易。
+  - icon: 🔑
+    title: 使用 TypeScript
+    details: 严格的 TypeScript 类型
+---
+<div align="center" style="margin-top: 50px">
+<img src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&size=30&pause=1000&center=true&vCenter=true&width=435&lines=%E6%84%BF%E6%88%91%E5%A6%82%E9%95%BF%E9%A3%8E%EF%BC%8C%E6%B8%A1%E5%90%9B%E8%A1%8C%E4%B8%87%E9%87%8C%E3%80%82" />
+
+</div>
+<!-- Placeholder -->

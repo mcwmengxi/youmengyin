fb34aa897d6bc835e8d6a7dfd2210bc1800d3841
mcwmengxi <1395568275@qq.com>
2022-11-08 20:05:05 +0800

    config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -26,5 +26,16 @@ export default defineConfig({
                 facetFilters: ['language:cn']
             }
         },
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+            { icon: 'twitter', link: '...' },
+            // You can also add custom icons by passing SVG as string:
+            {
+              icon: {
+                svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+              },
+              link: '...'
+            }
+          ]
     }
 })
